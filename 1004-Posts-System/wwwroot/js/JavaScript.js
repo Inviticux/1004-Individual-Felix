3f7333cc86c251e59339f0d098c5db37c361cb15
Pilchevski <felix.pendered@students.plymouth.ac.uk>
2023-05-14 17:17:01 +0100

    Adding the Whole Project to the Individual Repo

---
+++ 1004-Posts-System/wwwroot/js/JavaScript.js
@@ -0,0 +1,35 @@
+﻿//the buttons
+const postbutton = document.querySelector(".textformbutton");
+const likebutton = document.querySelector(".likebutton");
+const dislikebutton = document.querySelector(".dislikebutton");
+const keepbutton = document.querySelector(".keepbutton");
+
+//likes dislikes
+let likes = 0;
+let dislikes = 0;
+
+//other elements
+const labelText = document.querySelector(".textpost-form label");
+const texttoreplace = document.querySelector(".textofpost");
+const textarea = document.querySelector('textpost');
+
+postbutton.addEventListener("click", function () {
+    labelText.innerHTML = "Submitted";
+    const textareatext = document.getElementById("textpost").value;
+    texttoreplace.innerHTML = textareatext;
+    textarea.value = "";
+})
+
+likebutton.addEventListener("click", function () {
+    likes = likes + 1;
+    likebutton.innerHTML = "Likes: " + likes;
+})
+
+dislikebutton.addEventListener("click", function () {
+    dislikes = dislikes + 1;
+    dislikebutton.innerHTML = "Dislikes: " + dislikes;
+})
+
+keepbutton.addEventListener("click", function () {
+    keepbutton.innerHTML = "Commented";
+})
